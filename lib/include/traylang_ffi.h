2c9b2ea05dc3ca36bbba8a39a06abc05d6fb53ad
Guy John <guy@rumblesan.com>
2016-06-01 10:39:31 +0100

    Standardise FFI API even more

--- lib/include/traylang_ffi.h
+++ lib/include/traylang_ffi.h
@@ -3,12 +3,29 @@
 
 #include "traylang.h"
 #include "interpreter.h"
-#include "bclib/bstrlib.h"
-#include "bclib/list.h"
 #include "object.h"
 
-TrayLang *traylang_add_function(TrayLang *tray, char *name, c_func function);
+#include "bclib/bstrlib.h"
+#include "bclib/list.h"
 
-Object *get_obj(Interpreter *interpreter);
+typedef Interpreter TrayLangState;
+
+typedef Object TrayObject;
+
+typedef c_func TrayCFunction;
+
+TrayLang *traylang_add_function(TrayLang *tray, char *name, TrayCFunction function);
+
+TrayObject *traylang_get_obj(TrayLangState *state);
+
+TrayObject *traylang_new_cdata(TrayLangState *state, void *cdata);
+
+TrayObject *traylang_new_number(TrayLangState *state, double number);
+
+TrayObject *traylang_new_string(TrayLangState *state, bstring string);
+
+TrayObject *traylang_new_list(TrayLangState *state, List *list);
+
+TrayObject *traylang_new_nothing(TrayLangState *state);
 
 #endif
--- lib/src/traylang_ffi.c
+++ lib/src/traylang_ffi.c
@@ -1,4 +1,6 @@
 
+#include "traylang_ffi.h"
+
 #include "traylang.h"
 #include "interpreter.h"
 #include "interpreter_funcs.h"
@@ -6,7 +8,7 @@
 #include "object.h"
 
 TrayLang *traylang_add_function(TrayLang *tray, char *name, c_func function) {
-    Object *obj = interpreter_set_global(
+    TrayObject *obj = interpreter_set_global(
         tray->interpreter,
         bfromcstr(name),
         object_c_function(tray->interpreter, function)
@@ -17,7 +19,27 @@ error:
     return NULL;
 }
 
-Object *traylang_get_obj(Interpreter *interpreter) {
-    return get_obj(interpreter);
+TrayObject *traylang_get_obj(TrayLangState *state) {
+    return get_obj(state);
+}
+
+TrayObject *traylang_new_cdata(TrayLangState *state, void *cdata) {
+    return object_cdata(state, cdata);
+}
+
+TrayObject *traylang_new_number(TrayLangState *state, double number) {
+    return object_number(state, number);
+}
+
+TrayObject *traylang_new_string(TrayLangState *state, bstring string) {
+    return object_string(state, string);
+}
+
+TrayObject *traylang_new_list(TrayLangState *state, List *list) {
+    return object_list(state, list);
+}
+
+TrayObject *traylang_new_nothing(TrayLangState *state) {
+    return object_nothing(state);
 }
 
